aed6311c84846cc0bc414f6187b0f2babb739c49
priyadevarasetti123 <37026460+priyadevarasetti123@users.noreply.github.com>
2018-03-26 17:50:09 -0500

    Add files via upload

---
+++ db.php
@@ -0,0 +1,39 @@
+<?php
+
+Class DB
+{
+    private $servername = "den1.mysql4.gear.host";
+    private $username = "priyadb";
+    private $password = "Vg9tm463??04";
+    private $dbname = "priyadb";
+    private $conn = Null;
+
+    function connect()
+    {
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+            return Null;
+        }
+        return $this->conn;
+    }
+
+    function get_result($Statement)
+    {
+        $RESULT = array();
+        $Statement->store_result();
+        for ($i = 0; $i < $Statement->num_rows; $i++) {
+            $Metadata = $Statement->result_metadata();
+            $PARAMS = array();
+            while ($Field = $Metadata->fetch_field()) {
+                $PARAMS[] = &$RESULT[$i][$Field->name];
+            }
+            call_user_func_array(array($Statement, 'bind_result'), $PARAMS);
+            $Statement->fetch();
+        }
+        return $RESULT;
+    }
+}
+
+?>
